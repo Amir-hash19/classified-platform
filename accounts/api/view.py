28f2e7f1d9857cd3bca97e619f680cc1e3f941ac
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2026-09-03 14:51:41 +0330

    style: fix black and isort formatting

--- accounts/api/view.py
+++ accounts/api/view.py
@@ -2,8 +2,12 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status
-from rest_framework.generics import (CreateAPIView, ListAPIView,
-                                     RetrieveAPIView, UpdateAPIView)
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+)
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
